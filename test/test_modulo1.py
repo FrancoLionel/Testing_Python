0a8dcae3cf8ab06ad47794d9497d849f5686cf6a
FrancoLionel <fzalokarelosegui@gmail.com>
2025-11-29 13:41:30 +0100

    Testing basico

--- test/test_modulo1.py
+++ test/test_modulo1.py
@@ -3,7 +3,24 @@ import unittest
 from modulo1.funciones import esPalindromo
 
 class TestEsPalindromo(unittest.TestCase):
+    def test_con_numeros(self):
+        self.assertTrue(esPalindromo("99899"))
+        self.assertFalse(esPalindromo("56789"))
+    
+    def test_espacio_signos(self):
+        self.assertTrue(esPalindromo("l@a r*ut@a n#atúural"))
+        self.assertTrue(esPalindromo("m @ a d # a # m"))
+        
 
+    def test_palindromo_correcto(self):
+        self.assertTrue(esPalindromo("reconocer"))
+        self.assertTrue(esPalindromo("salas"))
+        self.assertTrue(esPalindromo("salas"))
+        self.assertTrue(esPalindromo("madam"))
+    
+    def test_palindromo_mayus_espacio(self):
+        self.assertTrue(esPalindromo("La Ruta Natural"))
+        self.assertTrue(esPalindromo("A Ti No Bonita"))
 
 if __name__ == "__main__":
     unittest.main()
